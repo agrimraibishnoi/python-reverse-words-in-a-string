43333920bd273b9f06994f76e6c3fdefcaa1c574
computermorris <72188348+computermorris@users.noreply.github.com>
2020-10-03 08:53:40 +0530

    Update Reversestring.py

--- Reversestring.py
+++ Reversestring.py
@@ -1,2 +1,27 @@
-txt = "Hello World" [::-1]
-print(txt)
+# Python code to reverse a string  
+# using reversed() 
+
+  
+# Function to reverse a string 
+
+def reverse(string): 
+
+    string = "".join(reversed(string)) 
+
+    return string 
+
+  
+
+s = str(input("Enter the string to be reverse : "))
+
+  
+
+print ("The original string  is : ",end="") 
+
+print (s) 
+
+  
+
+print ("The reversed string(using reversed) is : ",end="") 
+
+print (reverse(s))
